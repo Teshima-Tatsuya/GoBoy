d1ae3dcc59f55accef0a403008dff635ab721ccb
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-01-14 11:13:14 +0900

    fix Cartridge

--- pkg/gb/cartridge/cartridge.go
+++ pkg/gb/cartridge/cartridge.go
@@ -1,22 +1,48 @@
 package cartridge
 
-import "github.com/Teshima-Tatsuya/GoBoy/pkg/types"
+import (
+	"fmt"
+
+	"github.com/Teshima-Tatsuya/GoBoy/pkg/types"
+)
 
 type CartridgeType byte
 
+// ROM Type
+// @see https://gbdev.io/pandocs/The_Cartridge_Header.html#0147---cartridge-type
 const (
-	MBC_0                 = 0x00
-	MBC_1                 = 0x01
-	MBC_1_RAM             = 0x02
-	MBC_1_RAM_BATT        = 0x03
-	MBC_3_RAM_BATT        = 0x13
-	MBC_3_RAM_BATT_RTC    = 0x10
-	MBC_5                 = 0x19
-	MBC_5_RAM             = 0x1A
-	MBC_5_RAM_BATT        = 0x1B
-	MBC_5_RUMBLE          = 0x1C
-	MBC_5_RAM_RUMBLE      = 0x1D
-	MBC_5_RAM_BATT_RUMBLE = 0x1E
+	ROM = iota
+	MBC1
+	MBC2
+	MBC3
+	MBC4
+	MBC5
+	MBC6
+	MBC7
+)
+
+// ROM Size
+// @see https://gbdev.io/pandocs/The_Cartridge_Header.html#0148---rom-size
+const (
+	ROM_32KB int = (1 << (iota + 5)) * 1024
+	ROM_64KB
+	ROM_128KB
+	ROM_256KB
+	ROM_512KB
+	ROM_1024KB
+	ROM_2048KB
+	ROM_4096KB
+	ROM_8192KB
+)
+
+// RAM Size
+// @see https://gbdev.io/pandocs/The_Cartridge_Header.html#0149---ram-size
+const (
+	NO_RAM    int = 0 // also UNUSED
+	RAM_8KB   int = 8 * 1024
+	RAM_32KB  int = 32 * 1024
+	RAM_128KB int = 128 * 1024
+	RAM_64KB  int = 64 * 1024
 )
 
 // @see https://gbdev.io/pandocs/The_Cartridge_Header.html
@@ -29,7 +55,7 @@ type Cartridge struct {
 	// game supports SGB functions
 	SGBFlag bool
 	Type    byte
-	ROMSize byte
+	ROMSize int
 	RAMSize int
 	ROMData []byte
 }
@@ -46,27 +72,71 @@ func New(romData []byte) *Cartridge {
 		Title:        string(title),
 		CGBFlag:      cgbflag,
 		SGBFlag:      sgbflag,
-		Type:         romData[0x0147],
-		ROMSize:      romData[0x0148],
+		Type:         getType(romData[0x0147]),
+		ROMSize:      getRomSize(romData[0x0148]),
 		RAMSize:      getRamSize(romData[0x0149]),
 		ROMData:      romData,
 	}
 }
 
-func getRamSize(size byte) int {
-	switch size {
+func getRamSize(s byte) int {
+	switch s {
 	case 0x00, 0x01:
-		return 0
+		return NO_RAM
 	case 0x02:
-		return 8 * (2 << 10)
+		return RAM_8KB
 	case 0x03:
-		return 32 * (2 << 10)
+		return RAM_32KB
 	case 0x04:
-		return 128 * (2 << 10)
+		return RAM_128KB
 	case 0x05:
-		return 64 * (2 << 10)
+		return RAM_64KB
+	default:
+		errMsg := fmt.Sprintf("Non Supported RAM Type 0x%02X", s)
+		panic(errMsg)
 	}
-	return 0
+}
+
+func getType(t byte) byte {
+	switch t {
+	case 0x00:
+		return ROM
+	case 0x01, 0x02, 0x03:
+		return MBC1
+	case 0x05, 0x06:
+		return MBC2
+	case 0x0F, 0x10, 0x11, 0x12, 0x13:
+		return MBC3
+	case 0x15, 0x16, 0x17:
+		return MBC4
+	case 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E:
+		return MBC5
+	case 0x20:
+		return MBC6
+	case 0x22:
+		return MBC7
+	default:
+		errMsg := fmt.Sprintf("Non Supported type 0x%02X", t)
+		panic(errMsg)
+	}
+}
+
+func getRomSize(s byte) int {
+	return ROM_32KB << int(s)
+}
+
+func validateCheckSum(romData []byte) bool {
+	x := byte(0x00)
+
+	for i := 0x0134; i <= 0x014C; i++ {
+		x = x - romData[i] - byte(0x01)
+	}
+
+	if x != romData[0x014D] {
+		return false
+	}
+
+	return true
 }
 
 func (c *Cartridge) ReadByte(addr types.Addr) byte {
--- pkg/gb/rom/rom.go
+++ pkg/gb/rom/rom.go
@@ -1,4 +1,8 @@
 package rom
 
 type ROM struct {
+	// bank is 1-256
+	Bank byte
+	// buf is 16kb(0x4000-0x7FFF)
+	Buf [256][0x4000]byte
 }
