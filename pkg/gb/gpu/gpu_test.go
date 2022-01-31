e9bf29255ec9b6ef8e639d0317d015f96226d951
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-01-31 15:26:24 +0900

    test gpu

--- pkg/gb/gpu/gpu_test.go
+++ pkg/gb/gpu/gpu_test.go
@@ -9,21 +9,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func setupGPU() *GPU {
+func setupGPU1() *GPU {
 	g := New()
 	b := mock.NewMockBus()
 	bytes1 := []byte{0xFF, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E, 0xFF}
 	bytes2 := []byte{0x00, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E, 0xFF}
 
-	for tile := 0; tile < 384; tile++ {
-		addr := types.Addr(0x8800) + types.Addr(tile*16)
+	for tile := 0; tile < 255; tile++ {
+		baseAddr := types.Addr(0x8800) + types.Addr(tile*16)
 		if tile%2 == 0 {
 			for i := 0; i < len(bytes1); i++ {
-				b.WriteByte(addr, bytes1[i])
+				b.WriteByte(baseAddr+types.Addr(i), bytes1[i])
 			}
 		} else {
 			for i := 0; i < len(bytes2); i++ {
-				b.WriteByte(addr, bytes2[i])
+				b.WriteByte(baseAddr+types.Addr(i), bytes2[i])
 			}
 		}
 	}
@@ -32,9 +32,30 @@ func setupGPU() *GPU {
 	return g
 }
 
+func setupGPU2() *GPU {
+	g := New()
+	b := mock.NewMockBus()
+	bytes1 := []byte{0xFF, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E, 0xFF}
+	bytes2 := []byte{0x00, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E, 0xFF}
+
+	for tile := 0; tile < 255; tile++ {
+		baseAddr := types.Addr(0x8000) + types.Addr(tile*16)
+		if tile%2 == 0 {
+			for i := 0; i < len(bytes2); i++ {
+				b.WriteByte(baseAddr+types.Addr(i), bytes2[i])
+			}
+		} else {
+			for i := 0; i < len(bytes1); i++ {
+				b.WriteByte(baseAddr+types.Addr(i), bytes1[i])
+			}
+		}
+	}
+	g.Init(b, io.NewIRQ().Request)
+
+	return g
+}
 func TestGPU_loadTile(t *testing.T) {
 	t.Run("load tile", func(t *testing.T) {
-		g := setupGPU()
 
 		colors1 := [8][8]Color{
 			{LightGray, LightGray, LightGray, LightGray, LightGray, LightGray, LightGray, LightGray},
@@ -58,14 +79,28 @@ func TestGPU_loadTile(t *testing.T) {
 		}
 
 		t.Run("tile data 0", func(t *testing.T) {
+			g := setupGPU1()
 			g.LCDC.Data = 0x00 // tile data starts 0x8800
 			g.loadTile()
 
-			for i := 0; i < 384; i++ {
+			for i := 0; i < 255; i++ {
 				if i%2 == 0 {
-					assert.Equal(t, colors1, g.tiles[i])
+					assert.Equal(t, colors1, g.tiles[i].Data)
 				} else {
-					assert.Equal(t, colors2, g.tiles[i])
+					assert.Equal(t, colors2, g.tiles[i].Data)
+				}
+			}
+		})
+		t.Run("tile data 1", func(t *testing.T) {
+			g := setupGPU2()
+			g.LCDC.Data = 0x10 // tile data starts 0x8000
+			g.loadTile()
+
+			for i := 0; i < 255; i++ {
+				if i%2 == 0 {
+					assert.Equal(t, colors2, g.tiles[i].Data)
+				} else {
+					assert.Equal(t, colors1, g.tiles[i].Data)
 				}
 			}
 		})
