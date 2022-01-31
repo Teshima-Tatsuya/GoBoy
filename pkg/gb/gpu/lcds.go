c8284d4e3d9788be2da944ffb8b108d97f7fc8eb
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-02-01 07:23:00 +0900

    lcds

--- pkg/gb/gpu/lcds.go
+++ pkg/gb/gpu/lcds.go
@@ -17,11 +17,41 @@ func NewLCDS(data byte) *LCDS {
 	}
 }
 
+// Bit 6
 // LY STAT Interrupt source
 func (l *LCDS) LYC() bool {
 	return util.Bit(l.Data, 6) == 1
 }
 
+// Bit 5
+// OAM STAT Interrupt source
+func (l *LCDS) Mode2() bool {
+	return util.Bit(l.Data, 5) == 1
+}
+
+// Bit 4
+// OAM STAT Interrupt source
+func (l *LCDS) Mode1() bool {
+	return util.Bit(l.Data, 4) == 1
+}
+
+// Bit 3
+// OAM STAT Interrupt source
+func (l *LCDS) Mode0() bool {
+	return util.Bit(l.Data, 3) == 1
+}
+
+// Bit 2
+// OAM STAT Interrupt source
+func (l *LCDS) LYCLY() bool {
+	return util.Bit(l.Data, 2) == 1
+}
+
+// Bit 1-0
+// 0: HBlank
+// 1: VBlank
+// 2: Searching OAM
+// 3: Transferring Data to LCD Controller
 func (l *LCDS) Mode() Mode {
 	return Mode(l.Data & 0x03)
 }
