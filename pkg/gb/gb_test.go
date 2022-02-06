38f5b4862e28a784c637fe644da77d1403be877e
Tatsuya Teshima <terima666tatsuya@gmail.com>
2022-02-06 11:29:11 +0900

    add test

--- pkg/gb/gb_test.go
+++ pkg/gb/gb_test.go
@@ -13,7 +13,33 @@ func setup(romData []byte) *GB {
 	return NewGB(romData)
 }
 
-func TestGB_testroms(t *testing.T) {
+func test(name, filename string, frame int) {
+	testPath := "../../test/rom/"
+	outPath := "../../test/actual/"
+	romData, err := ioutil.ReadFile(testPath + name + "/" + filename + ".gb")
+	if err != nil {
+		panic(err)
+	}
+	gb := setup(romData)
+
+	for i := 0; i < frame; i++ {
+		gb.Step()
+	}
+	screen, _ := gb.Display()
+
+	if err := os.MkdirAll(outPath+name, 0777); err != nil {
+		panic(err)
+	}
+
+	file, _ := os.Create(outPath + name + "/" + filename + ".jpg")
+	defer file.Close()
+
+	if err := jpeg.Encode(file, screen, &jpeg.Options{100}); err != nil {
+		panic(err)
+	}
+}
+
+func TestGB_test_blargg(t *testing.T) {
 	tests := []struct {
 		name  string
 		file  string
@@ -22,7 +48,48 @@ func TestGB_testroms(t *testing.T) {
 		{"blargg/cpu_instrs", "cpu_instrs", 3200},
 		{"blargg/instr_timing", "instr_timing", 100},
 		{"blargg/interrupt_time", "interrupt_time", 100},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				err := recover()
+				if err != nil {
+					debug.Info("%s", err)
+				}
+			}()
+
+			test(tt.name, tt.file, tt.frame)
+		})
+	}
+}
+func TestGB_test_hello(t *testing.T) {
+	tests := []struct {
+		name  string
+		file  string
+		frame int
+	}{
 		{"helloworld", "hello", 100},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				err := recover()
+				if err != nil {
+					debug.Info("%s", err)
+				}
+			}()
+
+			test(tt.name, tt.file, tt.frame)
+		})
+	}
+}
+
+func TestGB_test_mooneye(t *testing.T) {
+	tests := []struct {
+		name  string
+		file  string
+		frame int
+	}{
 		{"mooneye-gb/acceptance/bits", "mem_oam", 100},
 		{"mooneye-gb/acceptance/bits", "reg_f", 100},
 		{"mooneye-gb/acceptance/bits", "unused_hwio-GS", 100},
@@ -32,7 +99,7 @@ func TestGB_testroms(t *testing.T) {
 		{"mooneye-gb/acceptance/oam_dma", "reg_read", 100},
 		{"mooneye-gb/acceptance/oam_dma", "sources-GS", 100},
 		{"mooneye-gb/acceptance/ppu", "hblank_ly_scx_timing-GS", 100},
-		{"mooneye-gb/acceptance/ppu", "intr_1_2_timing", 100},
+		{"mooneye-gb/acceptance/ppu", "intr_1_2_timing-GS", 100},
 		{"mooneye-gb/acceptance/ppu", "intr_2_0_timing", 100},
 		{"mooneye-gb/acceptance/ppu", "intr_2_mode0_timing_sprites", 100},
 		{"mooneye-gb/acceptance/ppu", "intr_2_mode0_timing", 100},
@@ -40,7 +107,7 @@ func TestGB_testroms(t *testing.T) {
 		{"mooneye-gb/acceptance/ppu", "intr_2_oam_ok_timing", 100},
 		{"mooneye-gb/acceptance/ppu", "stat_irq_blocking", 100},
 		{"mooneye-gb/acceptance/ppu", "stat_lyc_onoff", 100},
-		{"mooneye-gb/acceptance/ppu", "vblank_stat_inft-GS", 100},
+		{"mooneye-gb/acceptance/ppu", "vblank_stat_intr-GS", 100},
 		{"mooneye-gb/acceptance/serial", "boot_sclk_align-dmgABCmgb", 100},
 		{"mooneye-gb/acceptance/timer", "div_write", 100},
 		{"mooneye-gb/acceptance/timer", "rapid_toggle", 100},
@@ -55,6 +122,48 @@ func TestGB_testroms(t *testing.T) {
 		{"mooneye-gb/acceptance/timer", "tima_reload", 100},
 		{"mooneye-gb/acceptance/timer", "tima_write_reloading", 100},
 		{"mooneye-gb/acceptance/timer", "tma_write_reloading", 100},
+		{"mooneye-gb/acceptance", "add_sp_e_timing", 100},
+		{"mooneye-gb/acceptance", "boot_div-dmg0", 100},
+		{"mooneye-gb/acceptance", "boot_div-dmgABCmgb", 100},
+		{"mooneye-gb/acceptance", "boot_div-S", 100},
+		{"mooneye-gb/acceptance", "boot_div2-S", 100},
+		{"mooneye-gb/acceptance", "boot_hwio-dmg0", 100},
+		{"mooneye-gb/acceptance", "boot_hwio-dmgABCmgb", 100},
+		{"mooneye-gb/acceptance", "boot_hwio-S", 100},
+		{"mooneye-gb/acceptance", "boot_regs-dmg0", 100},
+		{"mooneye-gb/acceptance", "boot_regs-dmgABC", 100},
+		{"mooneye-gb/acceptance", "boot_regs-mgb", 100},
+		{"mooneye-gb/acceptance", "boot_regs-sgb", 100},
+		{"mooneye-gb/acceptance", "boot_regs-sgb2", 100},
+		{"mooneye-gb/acceptance", "call_cc_timing", 100},
+		{"mooneye-gb/acceptance", "call_cc_timing2", 100},
+		{"mooneye-gb/acceptance", "call_timing", 100},
+		{"mooneye-gb/acceptance", "call_cc_timing2", 100},
+		{"mooneye-gb/acceptance", "di_timing-GS", 100},
+		{"mooneye-gb/acceptance", "div_timing", 100},
+		{"mooneye-gb/acceptance", "ei_sequence", 100},
+		{"mooneye-gb/acceptance", "ei_timing", 100},
+		{"mooneye-gb/acceptance", "halt_ime0_ei", 100},
+		{"mooneye-gb/acceptance", "halt_ime0_nointr_timing", 100},
+		{"mooneye-gb/acceptance", "halt_ime1_timing", 100},
+		{"mooneye-gb/acceptance", "halt_ime1_timing2-GS", 100},
+		{"mooneye-gb/acceptance", "if_ie_registers", 100},
+		{"mooneye-gb/acceptance", "intr_timing", 100},
+		{"mooneye-gb/acceptance", "jp_cc_timing", 100},
+		{"mooneye-gb/acceptance", "jp_timing", 100},
+		{"mooneye-gb/acceptance", "ld_hl_sp_e_timing", 100},
+		{"mooneye-gb/acceptance", "ld_hl_sp_e_timing", 100},
+		{"mooneye-gb/acceptance", "oam_dma_restart", 100},
+		{"mooneye-gb/acceptance", "oam_dma_start", 100},
+		{"mooneye-gb/acceptance", "oam_dma_timing", 100},
+		{"mooneye-gb/acceptance", "pop_timing", 100},
+		{"mooneye-gb/acceptance", "push_timing", 100},
+		{"mooneye-gb/acceptance", "rapid_di_ei", 100},
+		{"mooneye-gb/acceptance", "ret_cc_timing", 100},
+		{"mooneye-gb/acceptance", "ret_timing", 100},
+		{"mooneye-gb/acceptance", "reti_intr_timing", 100},
+		{"mooneye-gb/acceptance", "reti_timing", 100},
+		{"mooneye-gb/acceptance", "rst_timing", 20},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -65,29 +174,29 @@ func TestGB_testroms(t *testing.T) {
 				}
 			}()
 
-			testPath := "../../test/rom/"
-			outPath := "../../test/actual/"
-			romData, err := ioutil.ReadFile(testPath + tt.name + "/" + tt.file + ".gb")
-			if err != nil {
-				panic(err)
-			}
-			gb := setup(romData)
+			test(tt.name, tt.file, tt.frame)
+		})
+	}
+}
 
-			for i := 0; i < tt.frame; i++ {
-				gb.Step()
-			}
-			screen, _ := gb.Display()
+func TestGB_test_temp(t *testing.T) {
+	tests := []struct {
+		name  string
+		file  string
+		frame int
+	}{
+		{"mooneye-gb/acceptance", "rst_timing", 20},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				err := recover()
+				if err != nil {
+					debug.Info("%s", err)
+				}
+			}()
 
-			if err := os.MkdirAll(outPath+tt.name, 0777); err != nil {
-				panic(err)
-			}
-
-			file, _ := os.Create(outPath + tt.name + "/" + tt.file + ".jpg")
-			defer file.Close()
-
-			if err := jpeg.Encode(file, screen, &jpeg.Options{100}); err != nil {
-				panic(err)
-			}
+			test(tt.name, tt.file, tt.frame)
 		})
 	}
 }
